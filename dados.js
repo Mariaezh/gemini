7c1dc283c903e02026941440cd9771088934e969
Maria E Zamora Hernandez <airamanelezh@gmail.com>
2024-09-08 21:35:28 -0300

    Atulização de minha base de dados

---
+++ dados.js
@@ -0,0 +1,54 @@
+let definicao = {
+    titulo: "Distrofia Muscular de Duchenne (DMD)",
+    descricao: "É uma doença neuromuscular genética, que se caracteriza como um distúrbio degenerativo progressivo e irreversível no tecido muscular, em especial a musculatura esquelética, que recobre totalmente o esqueleto e está presa aos ossos. A doença também afeta a musculatura cardíaca e o sistema nervoso",
+    link: "https://www.pfizer.com.br/sua-saude/doencas-raras/distrofia-muscular-de-duchenne",
+},
+const sintomasDuchenne = {
+    titulo: "Sintomas da Distrofia muscular de Duchenne",
+    descricao: [
+      {
+        faixaEtaria: "0-4 anos",
+        sintomas: [
+          "Inflamação logo após o nascimento;",
+          "Fibrose muscular (cicatrização) observada a partir de 1 ano de idade;",
+          "Atrasos motores;",
+          "Outros atrasos, por exemplo, fala."
+        ]
+      },
+      {
+        faixaEtaria: "5-7 anos",
+        sintomas: [
+          "Fraqueza muscular progressiva;",
+          "Aumento de panturrilhas;",
+          "Andar na ponta dos pés;",
+          "Dificuldade em pé de supino;",
+          "Acúmulo de gordura no músculo." 
+        ]
+      },
+      {
+        faixaEtaria: "8-11 anos",
+        sintomas: [
+            "Atrasos de marcos motores;",
+            "Diminuição da capacidade de caminhada;",
+            "Uso de cadeira de rodas em meio período."
+        ]
+      },
+      {
+        faixaEtaria: "12-19 anos",
+        sintomas: [
+            "Redução da função do membro superior, pulmão e coração;",
+            "Perda de deambulação;",
+            "Suporte ventilatório muitas vezes necessário;",
+            "Incapaz de realizar atividades de vida diária."
+        ]
+      },
+      {
+        faixaEtaria: "Adultos",
+        sintomas: [
+            "Aumento do problema cardíaco;",
+            "Insuficiência cardíaca;",
+            "Expectativa de vida severamente reduzida.".
+        ]
+      }
+    ]
+    };
